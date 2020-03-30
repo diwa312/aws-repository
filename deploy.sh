cb67c6e68bb5176e1bf89234c7e1f2911794a872
Diwakar Reddy <cdiwakarreddy312@gmail.com>
2020-03-30 07:19:17 +0530

    updated content

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ i am from devlopment branch
 this is dr branch 
 hiiii
 date
+time is 10.30 pm
 
---
+++ test.py
@@ -0,0 +1,5 @@
+hi 
+this is diwakar 
+and i am using gitbash in my pc for practicing 
+and let us practice git pull git fetech
+
